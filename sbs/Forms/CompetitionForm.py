391556a729509ceea82fe4756518369eb7bc1cf9
Halil BOZDAĞ <75361612+HalilBozdag@users.noreply.github.com>
2021-02-16 13:44:31 +0300

    Müsabaka genel türü select eklenmesi yazım yanlışlarının düzetilmesi

--- sbs/Forms/CompetitionForm.py
+++ sbs/Forms/CompetitionForm.py
@@ -10,13 +10,15 @@ class CompetitionForm(ModelForm):
 
         fields = (
             'name', 'startDate', 'finishDate', 'compType', 'eventPlace',
-            'registerStartDate', 'registerFinishDate',)
+            'registerStartDate', 'registerFinishDate','compGeneralType')
 
         labels = {'name': 'İsim', 'startDate': 'Başlangıç Tarihi', 'finishDate': 'Bitiş Tarihi',
                   'eventPlace': 'Etkinlik Yeri',
                   'registerStartDate': 'Ön Kayıt Başlangıç Tarihi',
                   'registerFinishDate': 'Ön Kayıt Bitiş Tarihi',
-                  'compType': 'Türü', }
+                  'compType': 'Türü',
+                  'compGeneralType': 'Genel Türü',
+                  }
 
         widgets = {
 
@@ -42,5 +44,7 @@ class CompetitionForm(ModelForm):
 
             'compType': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
                                             'style': 'width: 100%; '}),
+            'compGeneralType': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
+                                                   'style': 'width: 100%; '}),
 
         }
