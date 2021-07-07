b73c3657b7a34b04b2d94733051e4f07161fab51
Fatih Berktug <fatihberktug>
2021-07-07 23:10:02 +0300

    HIZ PROBLEMİ nedeni ile filtreleme geri alındı

--- sbs/Views/CompetitionViews.py
+++ sbs/Views/CompetitionViews.py
@@ -967,11 +967,6 @@ def antrenor_ajax(request):
 
         beka = []
         for item in Coach.objects.all():
-            clups = SportsClub.objects.filter(coachs=item)
-            clupsPk = []
-            for clup in clups:
-                clupsPk.append(clup.pk)
-            if Athlete.objects.filter(licenses__sportsClub_id__in=clupsPk) or Athlete.objects.filter(licenses__coach=item ):
                 data = {
                     'pk': item.pk,
                     'name': item.user.get_full_name(),
